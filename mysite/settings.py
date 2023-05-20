483029128205d2b78f5914965c264b2918475201
Linyi Li <linyili@andrew.cmu.edu>
2023-05-20 15:05:45 -0400

    update static files setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "django.contrib.staticfiles.storage.StaticFilesStorage"
 ]
 
 ROOT_URLCONF = "mysite.urls"
@@ -111,7 +111,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "static/"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
