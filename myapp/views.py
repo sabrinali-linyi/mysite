1905a8e5e376a4a9dd653699f5c1bcee2c49c3d3
Linyi Li <linyili@andrew.cmu.edu>
2023-05-20 14:52:42 -0400

    update views

--- myapp/views.py
+++ myapp/views.py
@@ -2,19 +2,22 @@ from django.http import JsonResponse
 import json
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.staticfiles import finders
 
 def screener_form(request):
-    screener = open("myapp/static/screener.json").read()
-    form = json.loads(screener)
-    content = form.get("content", {})
-    context = {
-        'sections': content.get('sections', {}),
-        'display_name': content.get('display_name', '')
-    }
-    for index, sec in enumerate(context['sections']):
-        sec['total_questions'] = len(sec['questions'])
-        context['sections'][index] = sec
-    return render(request, 'screener_form.html', context)
+    screener_path = finders.find('myapp/screener.json')
+    with open(screener_path) as f:
+        screener = f.read()
+        form = json.loads(screener)
+        content = form.get("content", {})
+        context = {
+            'sections': content.get('sections', {}),
+            'display_name': content.get('display_name', '')
+        }
+        for index, sec in enumerate(context['sections']):
+            sec['total_questions'] = len(sec['questions'])
+            context['sections'][index] = sec
+        return render(request, 'screener_form.html', context)
 
 @csrf_exempt
 def screener_endpoint(request):
@@ -23,38 +26,40 @@ def screener_endpoint(request):
         answers = data.get('answers', [])
         
         # Retrieve the domain mapping from the json file
-        domains = open("myapp/static/domains.json").read()
-        domain_mapping = json.loads(domains)
-        question_to_domain = {}
-        for dict in domain_mapping:
-            question_to_domain[dict["question_id"]] = question_to_domain.get(dict["question_id"], []).append(dict["domain"])
-        domain_scores = {}
-        
-        # Calculate the total score for each domain
-        for answer in answers:
-            value = answer.get('value', 0)
-            question_id = answer.get('question_id', '')
+        domains_path = finders.find('myapp/domains.json')
+        with open(domains_path) as f:
+            domains = open("myapp/static/domains.json").read()
+            domain_mapping = json.loads(domains)
+            question_to_domain = {}
+            for dict in domain_mapping:
+                question_to_domain[dict["question_id"]] = question_to_domain.get(dict["question_id"], []).append(dict["domain"])
+            domain_scores = {}
             
-            # Find the domain for the question
-            domain = question_to_domain.get(question_id, None)
+            # Calculate the total score for each domain
+            for answer in answers:
+                value = answer.get('value', 0)
+                question_id = answer.get('question_id', '')
+                
+                # Find the domain for the question
+                domain = question_to_domain.get(question_id, None)
+                
+                if domain:
+                    domain_scores[domain] = domain_scores.get(domain, 0) + value
             
-            if domain:
-                domain_scores[domain] = domain_scores.get(domain, 0) + value
-        
-        assessments = []
-        
-        # Determine the Level-2 Assessments based on the total scores
-        for domain, total_score in domain_scores.items():
-            if domain == 'depression' and total_score >= 2:
-                assessments.append('PHQ-9')
-            elif domain == 'mania' and total_score >= 2:
-                assessments.append('ASRM')
-            elif domain == 'anxiety' and total_score >= 2:
-                assessments.append('PHQ-9')
-            elif domain == 'substance_use' and total_score >= 1:
-                assessments.append('ASSIST')
-        
-        response_data = {'results': assessments}
-        return JsonResponse(response_data)
+            assessments = []
+            
+            # Determine the Level-2 Assessments based on the total scores
+            for domain, total_score in domain_scores.items():
+                if domain == 'depression' and total_score >= 2:
+                    assessments.append('PHQ-9')
+                elif domain == 'mania' and total_score >= 2:
+                    assessments.append('ASRM')
+                elif domain == 'anxiety' and total_score >= 2:
+                    assessments.append('PHQ-9')
+                elif domain == 'substance_use' and total_score >= 1:
+                    assessments.append('ASSIST')
+            
+            response_data = {'results': assessments}
+            return JsonResponse(response_data)
 
     return JsonResponse({'error': 'Invalid request method.'}, status=405)
